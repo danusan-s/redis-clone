eee721ea8d7a874422c9fc17529a31a43d07d7c4
danusan-s <danusansugumar@gmail.com>
2025-02-27 16:51:11 +0800

    Make client adhere to same protocol

--- src/client.cpp
+++ src/client.cpp
@@ -1,11 +1,23 @@
-#include <cassert>
-#include <cstring>
-#include <iostream>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <netinet/ip.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <vector>
 
-const size_t k_max_msg = 4096;
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
 
 static int32_t read_full(int fd, char *buf, size_t n) {
   while (n > 0) {
@@ -33,70 +45,100 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
   return 0;
 }
 
-static int32_t query(int fd, const char *text) {
-  uint32_t len = (uint32_t)strlen(text);
+const size_t k_max_msg = 4096;
+
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+  uint32_t len = 4;
+  for (const std::string &s : cmd) {
+    len += 4 + s.size();
+  }
   if (len > k_max_msg) {
     return -1;
   }
-  // send request
+
   char wbuf[4 + k_max_msg];
-  memcpy(wbuf, &len, 4); // assume little endian
-  memcpy(&wbuf[4], text, len);
-  if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-    return err;
+  memcpy(&wbuf[0], &len, 4); // assume little endian
+  uint32_t n = cmd.size();
+  memcpy(&wbuf[4], &n, 4);
+  size_t cur = 8;
+  for (const std::string &s : cmd) {
+    uint32_t p = (uint32_t)s.size();
+    memcpy(&wbuf[cur], &p, 4);
+    memcpy(&wbuf[cur + 4], s.data(), s.size());
+    cur += 4 + s.size();
   }
+  return write_all(fd, wbuf, 4 + len);
+}
+
+static int32_t read_res(int fd) {
   // 4 bytes header
   char rbuf[4 + k_max_msg + 1];
   errno = 0;
   int32_t err = read_full(fd, rbuf, 4);
   if (err) {
-    std::cout << (errno == 0 ? "EOF" : "read() error") << std::endl;
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read() error");
+    }
     return err;
   }
+
+  uint32_t len = 0;
   memcpy(&len, rbuf, 4); // assume little endian
   if (len > k_max_msg) {
-    std::cout << "message too long" << std::endl;
+    msg("too long");
     return -1;
   }
+
   // reply body
   err = read_full(fd, &rbuf[4], len);
   if (err) {
-    std::cout << "read() error" << std::endl;
+    msg("read() error");
     return err;
   }
-  printf("server says: %.*s\n", len, &rbuf[4]);
+
+  // print the result
+  uint32_t rescode = 0;
+  if (len < 4) {
+    msg("bad response");
+    return -1;
+  }
+  memcpy(&rescode, &rbuf[4], 4);
+  printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
   return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
-    std::cout << "Error: " << strerror(errno) << std::endl;
-    return 1;
+    die("socket()");
   }
 
   struct sockaddr_in addr = {};
   addr.sin_family = AF_INET;
   addr.sin_port = ntohs(1234);
-  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
-
+  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
   int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-  if (rv < 0) {
-    std::cout << "Error: " << strerror(errno) << std::endl;
-    return 1;
+  if (rv) {
+    die("connect");
   }
 
-  int32_t err = query(fd, "hello1");
+  // build command from arguments
+  std::vector<std::string> cmd;
+  for (int i = 1; i < argc; ++i) {
+    cmd.push_back(argv[i]);
+  }
+  int32_t err = send_req(fd, cmd);
   if (err) {
     goto L_DONE;
   }
-  err = query(fd, "hello2");
+  err = read_res(fd);
   if (err) {
     goto L_DONE;
   }
+
 L_DONE:
   close(fd);
   return 0;
-
-  close(fd);
 }
